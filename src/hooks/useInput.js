dfb82cc6a0d34583c61f94a5c9b89dd9fdf9675e
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-23 16:39:36 +0700

    fix: use input and print console

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -4,10 +4,11 @@ function useInput(defaultValue = '') {
   const [value, setValue] = useState(defaultValue);
 
   function handleValueChange({ target }) {
+    console.log('target', target.value);
     setValue(target.value);
   }
 
-  return [value, setValue];
+  return [value, handleValueChange, setValue];
 }
 
 export default useInput;
